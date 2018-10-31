b38ac0c462919ceeabc87297e7642d3a538e226a
IT16029682 <krishan.kr1996@gmail.com>
2018-10-31 15:51:28 +0530

    add ajax

---
+++ mainajax.js
@@ -0,0 +1,53 @@
+
+
+$('#mybutton').click(function(e){ 
+
+
+alert($('#zip').val()[1]);
+
+
+$.ajax({
+  //url: baseurl + '/api/repair/break',
+  url: '/api/state/car/atlocation',
+  type: 'POST',
+  contentType: "application/json",
+  data:  JSON.stringify({
+  	"singleRecord": "True",
+  	"attributes":{	"zip2":$('#zip').val()[0],
+  					"zip3":$('#zip').val()[1],
+  					"zip4":$('#zip').val()[2],
+  					"zip5":$('#zip').val()[3],
+  					"hod":$('#homeowner').val(),
+  					"numchild":$('#numchild').val(),
+  					"income":$('#income').val(),
+  					"gender":$('#gender').val(),
+  					"wealth":$('#wealth').val(),
+  					"hv":$('#hv').val(),
+  					"icmed":$('#icmed').val(),
+  					"icavg":$('#icavg').val(),
+  					"ic15":$('#ic15').val(),
+  					"numprom":$('#numprom').val(),
+  					"ramntall":$('#ramnt').val(),
+  					"maxramnt":$('#maxramnt').val(),
+  					"lastgift":$('#lastgift').val(),
+  					"totalmonths":$('#totmonths').val(),
+  					"timelag":$('#timelag').val(),
+  					"avggift":$('#avggift').val()
+  				}
+  }),
+   
+  success: function(data){
+	  $('res').html( JSON.stringify( data ) );
+    
+  },
+  error: function(data) {
+	   $('res').html( JSON.stringify( data ) );
+  }
+  //dataType: 'jsonp'
+  //success: function(data) {
+  //   console.log(data);
+  //},
+});
+
+alert(data);
+});
