bf38d15c941a4a9c01e355a5b69e8a177d93c103
IT16029682 <krishan.kr1996@gmail.com>
2018-10-31 18:07:05 +0530

    add uploadfile ajax

--- mainajax.js
+++ mainajax.js
@@ -49,5 +49,30 @@ $.ajax({
   //},
 });
 
-alert(data);
+
 });
+
+
+
+
+
+
+$("form#formupload").submit(function(e) {
+	
+	alert($('#fileformat:checked').val());
+    e.preventDefault();    
+    var formData = new FormData(this);
+
+    $.ajax({
+        url: window.location.pathname,
+        type: 'POST',
+        data: formData,
+        success: function (data) {
+            alert(data);
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+});
+
