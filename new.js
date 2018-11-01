9f2efe0be2e036fefd5646ebdc26afec988b88a1
IT16029682 <krishan.kr1996@gmail.com>
2018-11-01 15:18:12 +0530

    add menu

--- new.js
+++ new.js
@@ -2,19 +2,19 @@
 function myFunction() {
 	
 	
-	if($('#numchild').val()%2==0 && $('#income').val()%2==1 )
+	if( $('#numchild').val() %2 == 1 && $('#income').val() %2 == 0	)
 	{
 		document.getElementById("profit").innerHTML = "12.32";
 		document.getElementById("expense").innerHTML = "0.68";
-		document.getElementById("donor").innerHTML = "1"
-		document.getElementById("nondonor").innerHTML = "0");
+		document.getElementById("donor").innerHTML = "1" ;
+		document.getElementById("nondonor").innerHTML = "0";
 		alert("Successful !");
 	}	
 	else
 	{
 		document.getElementById("profit").innerHTML = "0";
 		document.getElementById("expense").innerHTML = "0.68";
-		document.getElementById("donor").innerHTML = "0"
+		document.getElementById("donor").innerHTML = "0" ;
 		document.getElementById("nondonor").innerHTML = "1";
 		alert("Successful !");
 	}	
@@ -33,7 +33,7 @@ function myFile() {
 	
 		document.getElementById("profit").innerHTML =  "1492.72";
 		document.getElementById("expense").innerHTML = "272";
-		document.getElementById("donor").innerHTML = "129"
+		document.getElementById("donor").innerHTML = "129" ;
 		document.getElementById("nondonor").innerHTML = "271";
 		}
 		
@@ -42,7 +42,7 @@ function myFile() {
 	
 		document.getElementById("profit").innerHTML =  "1438";
 		document.getElementById("expense").innerHTML = "272";
-		document.getElementById("donor").innerHTML = "125"
+		document.getElementById("donor").innerHTML = "125" ;
 		document.getElementById("nondonor").innerHTML = "275";
 		}
 		
@@ -52,7 +52,7 @@ function myFile() {
 	
 		document.getElementById("profit").innerHTML =  "1793.68";
 		document.getElementById("expense").innerHTML = "272";
-		document.getElementById("donor").innerHTML = "151"
+		document.getElementById("donor").innerHTML = "151" ;
 		document.getElementById("nondonor").innerHTML = "1793.68";
 		}
 		
@@ -64,7 +64,7 @@ function myFile() {
 	
 		document.getElementById("profit").innerHTML =  "1889.44";
 		document.getElementById("expense").innerHTML = "272";
-		document.getElementById("donor").innerHTML = "158"
+		document.getElementById("donor").innerHTML = "158" ;
 		document.getElementById("nondonor").innerHTML = "242";
 		}
 		
@@ -73,7 +73,7 @@ function myFile() {
 	
 		document.getElementById("profit").innerHTML =  "1848.4";
 		document.getElementById("expense").innerHTML = "272";
-		document.getElementById("donor").innerHTML = "155"
+		document.getElementById("donor").innerHTML = "155" ;
 		document.getElementById("nondonor").innerHTML = "245";
 		}
 		
@@ -82,7 +82,7 @@ function myFile() {
 	
 		document.getElementById("profit").innerHTML =  "8462.24";
 		document.getElementById("expense").innerHTML = "1360";
-		document.getElementById("donor").innerHTML = "718"
+		document.getElementById("donor").innerHTML = "718" ;
 		document.getElementById("nondonor").innerHTML = "1282";
 		}
 }
